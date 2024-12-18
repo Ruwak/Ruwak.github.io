6efcacdf471c4136274d263d259bbee2a016861a
Ruwak <150847546+Ruwak@users.noreply.github.com>
2024-12-18 09:34:05 -0800

    SW

--- TVMazeApp/apiexample.js
+++ TVMazeApp/apiexample.js
@@ -149,6 +149,7 @@ if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
     navigator.serviceWorker.register('TVMazeApp/sw.js').then(function(registration) {
       console.log('Service Worker registered with scope:', registration.scope);
+      
     }, function(error) {
       console.log('Service Worker registration failed:', error);
     });
