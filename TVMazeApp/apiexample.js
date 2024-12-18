3b51fec65a790a1cb1bcc2ad3c30eb076fb4f73f
Ruwak <150847546+Ruwak@users.noreply.github.com>
2024-12-18 09:25:51 -0800

    asd

--- TVMazeApp/apiexample.js
+++ TVMazeApp/apiexample.js
@@ -147,7 +147,7 @@ alt="${data.name}">
 // load the service worker
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('sw.js').then(function(registration) {
+    navigator.serviceWorker.register('TVMazeApp/sw.js').then(function(registration) {
       console.log('Service Worker registered with scope:', registration.scope);
     }, function(error) {
       console.log('Service Worker registration failed:', error);
